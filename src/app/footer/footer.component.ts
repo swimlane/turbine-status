6d6ba321c6ece0e246769799fb8d5bf371b2e28b
Anirban Banerjee <anirban.band@gmail.com>
2025-01-16 22:21:23 +0530

    Added footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { labels, urls } from '../../config';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  labels = labels;
+  urls = urls;
 }
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,55 @@
 export const labels = {
-  "bannerTitle": "Welcome to the Swimlane Status Page",
-  "bannerSubtitle": "Below you'll find status information for Swimlane's Cloud-native Turbine platform",
-  "supportButton": "Go to Support Portal"
-}
+  bannerTitle: 'Welcome to the Swimlane Status Page',
+  bannerSubtitle:
+    "Below you'll find status information for Swimlane's Cloud-native Turbine platform",
+  supportButton: 'Go to Support Portal',
+  copyright: '© 2022 Swimlane Inc. All rights reserved.',
+  gdrp: 'GDRP',
+  privacyPolicy: 'Privacy Policy',
+  termsOfUse: 'Terms of Use',
+  csr: 'CSR',
+};
 
 export const urls = {
-  "support": "https://swimlane.freshdesk.com/support/login"
-}
+  support: 'https://swimlane.freshdesk.com/support/login',
+  facebook: '',
+  github: '',
+  linkedin: 'https://www.linkedin.com/company/swimlane',
+  twitter: 'https://twitter.com/swimlane',
+  youtube: 'https://www.youtube.com/channel/UCs6qP3QXytOsCD1TRIw0EYg',
+  gdrp: '',
+  privacyPolicy: '',
+  termsOfUse: '',
+  csr: '',
+};
+
+export const regions = [
+  {
+    name: 'US East',
+    url: '',
+  },
+  {
+    name: 'US West',
+    url: '',
+  },
+  {
+    name: 'Asia Pacific (Japan)',
+    url: '',
+  },
+  {
+    name: 'Europe (UK)',
+    url: '',
+  },
+  {
+    name: 'Asia Pacific (Singapore)',
+    url: '',
+  },
+  {
+    name: 'Europe (Germany)',
+    url: '',
+  },
+  {
+    name: 'Asia Pacific (Australia)',
+    url: '',
+  },
+];
