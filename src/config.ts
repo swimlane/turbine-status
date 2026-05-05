6abb6eee09f5dc25e6f98ee71416e4eb9220573f
Ryan Knauer <ryan@bageltechnologies.com>
2026-05-05 13:25:46 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -40,7 +40,7 @@ export const regions: Array<Region> = [
   },
   {
     name: 'Canada (CA Central 1)',
-    url: 'https://cacentral1.swimlanestatus.com',
+    url: 'https://turbinecanadacentral.statuspage.io/',
     appdomain: 'ca1.swimlane.app',
   },
   {
